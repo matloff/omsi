2fd9f9b70cc4e92d7de6345c97eb6052af554654
FrancoisDemoullin <f.demoullin@gmail.com>
2016-02-03 18:17:52 -0800

    Refactoring Part 3. Improvements when sending file to Server

--- ClientGlobals.py
+++ ClientGlobals.py
@@ -3,5 +3,5 @@ import socket
 gHost = socket.gethostbyname(socket.gethostname())
 gPort = 5000
 
-gStudentEmail = "someoneelse@ucdavis.edu"
-gStudentHomeDirectory = "StudentHomeDirectoryIDONTEXISTLETSSEEWHATHAPPENS"
+gStudentEmail = "fdemoullin@ucdavis.edu"
+gStudentHomeDirectory = "StudentHomeDirectory"
