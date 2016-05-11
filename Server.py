d7b10683cd03eea547c981eafd19bd3c5e5b11bc
RylanSchaeffer <rylanschaeffer@gmail.com>
2016-05-10 20:38:57 -0700

    Removed number of current clients

--- Server.py
+++ Server.py
@@ -3,9 +3,6 @@ __author__ = 'fdemoullin'
 # server script
 # should be run without interruption on professor's machine
 
-import sys
-import select
-import socket
 import thread
 
 # import modules that contain functions called by a client
@@ -30,23 +27,16 @@ def main():
 
     while True:
         # server is now ready to accept connections
-        print "Number of Current Connections: %s" % ServerGlobals.gNumCurrentClients
         print "Number of Total Connections: %s" % ServerGlobals.gNumTotalClients
 
         # block until connection arrives
         lClientsocket, lAddr = lServerSocket.accept()
         print "Connection detected at:", lAddr
 
-        # increase number of current connections
-        ServerGlobals.gNumCurrentClientsLock.acquire()
-        ServerGlobals.gNumCurrentClients += 1
-        ServerGlobals.gNumCurrentClientsLock.release()
-
         # increase number of total connections
         ServerGlobals.gNumTotalClientsLock.acquire()
         ServerGlobals.gNumTotalClients += 1
         ServerGlobals.gNumTotalClientsLock.release()
-
 
         # start new thread (function, args_tuple) for each new connection
         # lAddr is not used right now. However python syntax requires a tuple as input parameters to a new thread
