f4e203983d950d84b427b97dd71165ad00a29324
Monte Misa <montemisa@gmail.com>
2016-05-15 19:01:23 -0700

    Added ability for -flags option

--- OMSIQuestion.py
+++ OMSIQuestion.py
@@ -1,9 +1,10 @@
 class OMSIQuestion:
-	def __init__(self,question,number,filetype='.txt'):
+	def __init__(self,question,number,filetype='.txt',flags=""):
 		self.number = number
 		self.filetype = filetype
 		self.answer = "Write your answer here..."
 		self.question = question
+		self.flags = flags
 
 	def getQuestion(self):
 		return self.question
@@ -19,4 +20,7 @@ class OMSIQuestion:
 
 	def setAnswer(self,ans):
 		self.answer = ans
+
+	def getFlags(self):
+		return self.flags
 
--- utility.py
+++ utility.py
@@ -1,6 +1,7 @@
 #Utility tools for the application
 from OMSIQuestion import *
 import pdb
+import shlex
 
 #Will return an array of OMSIQuestion objects
 def ParseQuestions(filename):
@@ -25,7 +26,8 @@ def ParseQuestions(filename):
 				firstQuestion = True
 				
 				filetype = '.txt'
-				words = line.split()
+				flags = ""
+				words = shlex.split(line)
 
 				for i in range(len(words)):
 					if words[i] == '-ext':
@@ -35,12 +37,20 @@ def ParseQuestions(filename):
 							print("Setting type to {0}".format(words[i+1]))
 							filetype = words[i+1]
 						i+= 1
+					if words[i] == '-flags':
+						if i+1 >= len(words):
+							print("Error! Unexpected end of arguments...")
+						else:
+							fl = words[i+1]
+							print("Setting flags to {0}".format(fl))
+							flags = fl
+
 
 				line = f.readline()
 				while line and 'DESCRIPTION' not in line and 'QUESTION' not in line:
 					question += line
 					line = f.readline()
-				q = OMSIQuestion(question,len(questions),filetype) 
+				q = OMSIQuestion(question,len(questions),filetype,flags) 
 				questions.append(q)
 				question = ""
 			else:
